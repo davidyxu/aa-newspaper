5633a2c5cdd16311bc5246d9d621c6c0bf9b847a
David Xu <dvdyxu@gmail.com>
2013-04-10 09:04:02 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewspaperSubscription::Application.config.secret_token = '014df90765313a2be550fc333b8e2fd9f1f94882194c447cc463181633d2f687a0dd36791bc0aa2102047bbff69b7cfc28a5499da30a2248c9ce28254299e570'
